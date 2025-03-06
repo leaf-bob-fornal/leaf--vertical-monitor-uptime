4e69499570030d7908dc51f1e8b52cc375d4c470
Bob Fornal <bob.fornal@leafhome.com>
2025-03-05 22:39:46 -0500

    Storing with date of run

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ process.on('SIGINT', () => {
 processSites(core.DOMAINS);
 
 function processSites(sites) {
-  fs.writeFile('results.csv', 'site,time,status\n', () => {});
+  const datePattern = getFormattedDate();
+  fs.writeFile(`results--${datePattern}.csv`, 'site,time,status\n', () => {});
 
   intervalId = setInterval(() => {
 
@@ -30,6 +31,15 @@ function processSites(sites) {
       }
     }
   }, core.INTERVAL);
+}
+
+function getFormattedDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
 }
 
 function checkWebsite(url, callback) {
